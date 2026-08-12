901c0a78b354e452bf02dbccb3b91cb28286b6d9
Riwayat-classic <riwayat.businesses@gmail.com>
2026-08-12 09:41:56 +0500

    Update page.tsx

--- app/(dashboard)/dashboard/employees/[id]/page.tsx
+++ app/(dashboard)/dashboard/employees/[id]/page.tsx
@@ -26,11 +26,32 @@ if (!employee) {
         Employee Details
       </h1>
 
-      <p>Employee ID:</p>
+     <div className="rounded-lg border bg-white p-6 space-y-3">
+  <p>
+    <strong>Employee Code:</strong>{" "}
+    {employee.employeeCode}
+  </p>
 
-      <div className="rounded-lg border bg-white p-6">
-        {id}
-      </div>
+  <p>
+    <strong>Full Name:</strong>{" "}
+    {employee.fullName}
+  </p>
+
+  <p>
+    <strong>Phone:</strong>{" "}
+    {employee.phone}
+  </p>
+
+  <p>
+    <strong>CNIC:</strong>{" "}
+    {employee.cnic || "Not provided"}
+  </p>
+
+  <p>
+    <strong>Address:</strong>{" "}
+    {employee.address || "Not provided"}
+  </p>
+</div>
     </div>
   );
 }
